3ffd0c4de8f9173d0b3f78443fe7c57337575edd
felipe-zg <felipe_zeba@outlook.com>
2021-11-09 20:06:30 -0600

    test: ensure RemoteAddAccount calls add method with correct url

---
+++ DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,45 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Felipe Zêba on 11/9/21.
+//
+
+import XCTest
+
+class RemoteAddAccount {
+    var url: URL
+    var httpPostClient: HttpPostClient
+    
+    init(url: URL, httpPostClient: HttpPostClient) {
+        self.url = url
+        self.httpPostClient = httpPostClient
+    }
+    
+    func add() {
+        httpPostClient.post(url: url)
+    }
+}
+
+protocol HttpPostClient {
+    func post(url: URL)
+}
+
+class RemoteAddAccountTests: XCTestCase {
+
+    func test_add_should_call_httpPostClient_with_correct_url() {
+        let url = URL(string: "https://anuurl.com")!
+        let httpPostClientSpy = HttpPostClientSpy()
+        let sut = RemoteAddAccount(url: url, httpPostClient: httpPostClientSpy)
+        sut.add()
+        XCTAssertEqual(httpPostClientSpy.url, url)
+    }
+
+    class HttpPostClientSpy: HttpPostClient {
+        var url: URL?
+        
+        func post(url: URL) {
+            self.url = url
+        }
+    }
+}
